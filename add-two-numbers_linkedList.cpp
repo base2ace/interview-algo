2c275c8c34aca1ea080614d5f4c370ca196bccf5
base2ace <100696702+base2ace@users.noreply.github.com>
2023-08-14 11:44:40 +0530

    Update add-two-numbers_linkedList.cpp

--- add-two-numbers_linkedList.cpp
+++ add-two-numbers_linkedList.cpp
@@ -40,34 +40,43 @@ It is guaranteed that the list represents a number that does not have leading ze
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    // Create a dummy node to serve as the starting point of the answer linked list.
+    ListNode *answer = new ListNode(0);
+    ListNode *current = answer; // Pointer to the current node in the answer linked list.
+    int carry = 0; // Initialize carry to 0.
+    auto node1 = l1; // Pointer to traverse l1 linked list.
+    auto node2 = l2; // Pointer to traverse l2 linked list.
+    
+    // Traverse through both linked lists and handle carry until both lists are processed and carry is 0.
+    while (node1 != nullptr || node2 != nullptr || carry != 0) {
+        // Extract the digits from the current nodes of l1 and l2.
+        int digit1 = node1 != nullptr ? node1->val : 0;
+        int digit2 = node2 != nullptr ? node2->val : 0;
 
-        ListNode *answer = new ListNode(0);
-        ListNode *current = answer;
-        int carry = 0;
-        auto node1 = l1;
-        auto node2 = l2;
+        // Calculate the sum of digits from l1, l2, and carry.
+        int sum = digit1 + digit2 + carry;
         
-        while(node1 != nullptr || node2 != nullptr || carry !=0 )
-        {
-            int digit1 = node1 != nullptr ? node1->val : 0;
-            int digit2 = node2 != nullptr ? node2->val : 0;
+        // Calculate the new digit for the current position.
+        auto digit = sum % 10;
 
-            int sum = digit1 + digit2 + carry;
-            auto digit = sum % 10;
-
-            // create a new node to store the new digit.
-            auto digitNode = new ListNode(digit);
-            current->next = digitNode;
-            current = digitNode;
-
-            carry = sum / 10;
-     
-            node1 = node1 != nullptr ? node1->next : nullptr;
-            node2 = node2 != nullptr ? node2->next : nullptr;
-        }
-
-        return answer->next;
+        // Create a new node to store the new digit in the answer linked list.
+        auto digitNode = new ListNode(digit);
         
+        // Link the new digit node to the current node in the answer linked list.
+        current->next = digitNode;
+        current = digitNode; // Move the current pointer to the newly added digit node.
+        
+        // Calculate the carry for the next iteration.
+        carry = sum / 10;
+
+        // Move to the next nodes in l1 and l2 if they are not nullptr.
+        node1 = node1 != nullptr ? node1->next : nullptr;
+        node2 = node2 != nullptr ? node2->next : nullptr;
     }
+
+    // The answer linked list starts from the node after the dummy node.
+    return answer->next;
+}
+
 };
